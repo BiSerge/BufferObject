fa1bf4be9bbae19e9522b8c08c457fd8d2d9ab4f
Сергей <bichevoi@gmail.com>
2016-01-31 16:08:41 +0600

    Добавил третий тест, поправил имеющиеся

--- BufferObjectTest/StorageTest.cs
+++ BufferObjectTest/StorageTest.cs
@@ -8,35 +8,51 @@ namespace BufferObjectTest
     public class StorageTest
     {
         [TestMethod]
-        public void AddGoods_IncreasingTheNumberGoods()
+        public void AddGoods_AddGoods_IncreasingTheNumberGoods() // При добавлении количество увеличивается на 1
         {
-            // arrange
+            // Организация
             int myMaxGoods = 10;
             Storage myStorage = new Storage(myMaxGoods, "SkladTest");
             Goods myTovar;
             int expected = myStorage.GetCount() + 1;
 
-            // act
+            // Действие
             myStorage.AddGoods(myTovar = new Goods());
 
-            // assert
+            // Утверждение
             int actual = myStorage.GetCount();
-            Assert.AreEqual(expected, actual, 0, "Товар не добавляется на склад!");
+            Assert.AreEqual(expected, actual, 0, "Количество товара не увеличивается!");
         }
 
         [TestMethod]
-        public void GetGoods_GetGoods()
+        public void AddGoods_MoreThanTheMaximumAmount_QuantityDoesNotIncrease()     // При добавлении товара количество не должно привышать максимума
         {
-            // arrange
+            // Организация
+            int myMaxGoods = 2;
+            Storage myStorage = new Storage(myMaxGoods, "SkladTest");
+            Goods myTovar;
+
+            // Действие
+            for (int i = 1; i < myMaxGoods + 2; i++)
+                myStorage.AddGoods(myTovar = new Goods());
+
+            // Утверждение
+            int actual = myStorage.GetCount();
+            Assert.AreEqual(myMaxGoods, actual, "Количество товара больше максимального!");
+        }
+
+        [TestMethod]
+        public void GetGoods_GetTheGoods_TheResultingProductIsIndoors() // Получение товара товара
+        {
+            // Организация
             int myMaxGoods = 10;
             Storage myStorage = new Storage(myMaxGoods, "SkladTest");
             Goods expected = new Goods();
-            //Goods expected = myTovar;
 
-            // act
+            // Действие
             myStorage.AddGoods(expected);
 
-            // assert
+            // Утверждение
             Goods actual = myStorage.GetGoods();
             Assert.AreEqual(expected, actual, "Товар не получен!");
         }
